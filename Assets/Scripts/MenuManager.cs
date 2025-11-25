1b8db969b9ab550b1d8a6f7733da5b766a321b51
Ayoub Sougrati <sougrati.a@northeastern.edu>
2025-11-25 15:06:14 -0500

    working buttons

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -1,22 +1,62 @@
 using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Policies;
+using System;
+
 
 public class MenuManager : MonoBehaviour
 {
     public GameObject startPage;
 
-    public GameObject[] agents;
+    public GameObject countdown;
+
+    public BehaviorParameters[] agents;
+
+    public Boolean tagger;
+
+    public Boolean runner;
+
 
     void Start()
     {
         startPage.SetActive(true);
+        Time.timeScale = 0f;
+
         foreach (var agent in agents)
-            agent.SetActive(false);
+            agent.gameObject.SetActive(false);
     }
+
+    public void PlayAsTagger()
+    {
+        tagger = true;
+        StartGame();
+    }
+
+    public void PlayAsRunner()
+    {
+        runner = true;
+        StartGame();
+    }
+
 
     public void StartGame()
     {
-        foreach (var agent in agents)
-            agent.SetActive(true);
+
         startPage.SetActive(false);
+
+        Time.timeScale = 1f;
+
+        foreach (var agent in agents)
+        {
+            agent.gameObject.SetActive(true);
+            if ((tagger && agent.gameObject.tag == "Tagger") || (runner && agent.gameObject.tag == "Runner"))
+            {
+                agent.BehaviorType = BehaviorType.HeuristicOnly;
+            } else
+            {
+                agent.BehaviorType = BehaviorType.InferenceOnly;
+            }
+
+        }
     }
 }
